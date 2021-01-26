211be713597ae20647db131bfdcbf1c41bfe5232
MURA199918 <muralisaranr18@gmail.com>
2021-01-26 13:26:30 +0530

    [Murali]Refactored to create payroll-form page to add employee

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import PayrollForm from './components/payrollform/PayrollForm';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Hello Employee Payroll Application
+      <Router>
+        <Switch>
+          <Route>
+            <PayrollForm/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
